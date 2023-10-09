0c1582d3e970763e08a1afc7cc9c17a9c3a3e4d5
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-10-09 18:13:00 +0200

    header logic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.sass'],
 })
 export class HeaderComponent {
-
+  totalIncome: number = 0;
+  totalExpense: number = 0;
+  total: number = this.totalIncome - this.totalExpense;
 }

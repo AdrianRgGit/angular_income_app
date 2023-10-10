90facc90d1d0273f3ec5bdcf1cbab39c1446ad5a
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-10-10 19:19:52 +0200

    dejo a medias el componente incomes

--- src/app/incomes/incomes.component.ts
+++ src/app/incomes/incomes.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movement } from '../movement.model';
+import { MovementService } from '../movements.service';
 
 @Component({
   selector: 'app-incomes',
   templateUrl: './incomes.component.html',
   styleUrls: ['./incomes.component.sass'],
 })
-export class IncomesComponent {
-  newIncome: number = 0;
+export class IncomesComponent implements OnInit {
+  incomes: Movement[] = [];
+
+  constructor(private movementService: MovementService) {}
+
+  ngOnInit(): void {
+    this.incomes = this.movementService.incomes;
+  }
 }

c48668172adf45709f538b3a0aa2bcebb4198507
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-10-10 19:09:19 +0200

    ya funciona el selector

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,17 +8,28 @@ import { MovementService } from '../movements.service';
   styleUrls: ['./form.component.sass'],
 })
 export class FormComponent {
+  // FIXME: No me funciona como booleano, echarle un vistazo
+  isIncome: string = 'false';
   reason: string = '';
   amount: number = 0;
 
   constructor(private movementService: MovementService) {}
 
   newMovement() {
-    if(!this.reason){
-      this.reason = "A default reason"
+    if (!this.reason) {
+      this.reason = 'A default reason';
     }
+    let originalAmount = this.amount;
+    console.log(this.isIncome);
+
+    if (this.isIncome == 'false') {
+      this.amount = -this.amount;
+    }
+
     let movement = new Movement(this.reason, this.amount);
+
+    this.amount = originalAmount;
     this.movementService.pushMovement(movement);
-    this.reason = ""
+    this.reason = '';
   }
 }
